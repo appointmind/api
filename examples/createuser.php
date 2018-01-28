0ff761c0cab8870dc3b1f5b308014df5c5f9dab7
Appointmind <appointmind@users.noreply.github.com>
2018-01-28 03:51:10 +0100

    Create createuser.php

---
+++ examples/createuser.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../../autoload.php';
+
+use Appointmind\User;
+
+
+$uri = '';
+$accessKey = '';
+$secretKey = '';
+
+
+$user = new User();
+$user->setUri($uri);
+$user->setAccessKey($accessKey);
+$user->setSecretKey($secretKey);
+$user->create([
+    'firstName' => 'Abby',
+    'lastName' => 'Normal',
+    'email' => 'abby.normal@example.com',
+    'password' => 'test',
+]);
